34a4ff473443b084d875045e6e1417616f08600c
Saurabh3521 <162089685+SaurabhTech3521@users.noreply.github.com>
2026-03-04 19:27:54 +0530

    Update remove_background.py

--- remove_background.py
+++ remove_background.py
@@ -8,8 +8,9 @@ st.title("AI Background Remover")
 uploaded_file = st.file_uploader("Upload an Image")
 
 if uploaded_file :
+    #  Open the input image
     img = Image.open(uploaded_file)
-
+    # Subheader for original image
     st.subheader("Original Image")
 
     st.image(img, width="stretch")
@@ -17,7 +18,7 @@ if uploaded_file :
     if st.button("Remove Background"):
         with st.spinner("Removing Background.....Please Wait !!"):
             output = remove(img)
-
+        # Subheader for Back_ground removed image
         st.subheader("Background Removed")
 
         st.image(output)
